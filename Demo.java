1d02566ba89fdbd190b2a3684918cd8df3618e11
cuda-unknown <soonyouwillknow34@gmail.com>
2026-06-08 15:33:11 +0530

    first day

---
+++ Demo.java
@@ -0,0 +1,29 @@
+class Demo {
+    public static void main(String[]args){
+        // System.out.println( "Hi There");
+        // System.out.print("hi me ");
+        // boolean n=true;
+        // int a=90;
+        // char c='m';
+        // String m="avgy";
+        // System.out.println(n+"\n" +a+"\n"+c+"\n" + m+"\n" );
+        int u=10,y=99,o=188;
+        // System.out.println(u+y);
+        if(u>y && u>o){
+            System.out.println(u);
+
+        }
+        else if(y>u && y>o){
+            System.out.println(y);
+        }
+            else{
+            System.out.println(o);
+        }
+        
+    }
+}
+
+
+
+
+
