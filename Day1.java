d7ef2670e373c248ede9c40cb8209b8c2ac01ccf
cuda-unknown <soonyouwillknow34@gmail.com>
2026-06-09 15:54:37 +0530

    Day 2

--- Day1.java
+++ Day1.java
@@ -1,1 +1,79 @@
+import java.util.Scanner;
 
+class Start {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter number");
+        int num =sc.nextInt();
+        System.out.println(num);
+
+        System.out.println("enter big number");
+        long many=sc.nextLong();
+        System.out.println(many);
+
+        sc.nextLine();
+
+        System.out.println("enter Char ");
+        char m=sc.next().charAt(0);
+        System.out.println(m);
+
+        System.out.println("enter Double ");
+        Double d=sc.nextDouble();
+        System.out.println(d);
+
+        sc.nextLine();
+
+        System.out.println("enter string ");
+        String name=sc.nextLine();
+        System.out.println(name);
+
+        System.out.println("enter Float ");
+        Float f=sc.nextFloat();
+        System.out.println(f);
+
+       
+
+        //Ternary 
+        int in=12;
+        String outting= (in<=10   ? "win" : "Loss");
+
+        System.out.println(outting);
+
+
+        //Switch
+
+        System.out.println("Input color");
+
+        String color = sc.nextLine();
+
+        switch (color) {
+            case "Red":
+                System.out.println("Stop");
+                break;
+
+            case "Yellow":
+                System.out.println("Wait");
+                break;
+
+            case "Green":
+                System.out.println("Go");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+
+        //For loop 
+        for(int i=0;i<10;i++){
+            //continue 
+            if(i>=9) break;
+            //break
+            else if (i==5)  continue;
+            else  System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
--- Demo.java
+++ Demo.java
@@ -1,24 +1,27 @@
+
 class Demo {
     public static void main(String[]args){
-        // System.out.println( "Hi There");
-        // System.out.print("hi me ");
-        // boolean n=true;
-        // int a=90;
-        // char c='m';
-        // String m="avgy";
-        // System.out.println(n+"\n" +a+"\n"+c+"\n" + m+"\n" );
+
+        System.out.println( "Hi There");
+        System.out.print("hi me ");
+        boolean n=true;
+        int a=90;
+        char c='m';
+        String m="avgy";
+        System.out.println(n+"\n" + a+"\n" + c+"\n" + m+"\n" );
         int u=10,y=99,o=188;
         // System.out.println(u+y);
         if(u>y && u>o){
             System.out.println(u);
-
         }
         else if(y>u && y>o){
             System.out.println(y);
         }
-            else{
+        else{
             System.out.println(o);
-        }
+        }        
+    
+     
         
     }
 }
